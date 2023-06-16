47ab8c7c84107600f57fd848b5c53653e279ade9
Hawkins <2398430768@qq.com>
2023-06-16 14:23:53 +0800

    更新ajax教程

--- 00.ajax入门/原生Ajax/server.js
+++ 00.ajax入门/原生Ajax/server.js
@@ -39,6 +39,17 @@ app.get('/jquery-get',(request,response)=>{
 
 });
 
+app.get('/axios-get',(request,response)=>{
+  // 设置响应头，设置允许跨域
+  response.setHeader('Access-Control-Allow-Origin','*')
+  // 设置响应体
+  // response.send('hello ajax jquery GET')
+  const data={name:'hawkins',number:'wafwef'}
+  response.send(data)
+
+
+});
+
 
 // 延时响应
 app.get('/delay',(request,response)=>{
@@ -67,6 +78,44 @@ app.all('/json-server',(request,response)=>{
     response.send(str)
 });
 
+// 测试跨域jsonp
+app.get('/jsonp-server',(request,response)=>{
+  const data={
+    name:'hello jsonp'
+  }
+  let str=JSON.stringify(data)
+  // 必须要返回一段函数,函数的参数就是返回的数据
+  response.end(`handle(${str})`)
+})
+
+// 测试jquery-jsonp
+app.get('/jsonp-server-jsonp',(request,response)=>{
+  const data={
+    name:'hello jsonp-jsonp'
+  }
+  let str=JSON.stringify(data)
+  // 接受callbak参数
+  let cb=request.query.callback
+  // 必须要返回一段函数,函数的参数就是返回的数据
+  response.end(`${cb}(${str})`)
+})
+// 测试用户是否存在
+app.all('/check-username',(request,response)=>{
+  const data={
+    exist:1,
+    msg:'用户们已存在'
+  }
+  let str=JSON.stringify(data)
+  // 必须要返回一段函数,函数的参数就是返回的数据
+  response.end(`handle(${str})`)
+})
+
+// 测试cors
+app.all('/check-cors',(request,response)=>{
+  //设置响应头，设置允许跨
+  response.setHeader("Access-Control-Allow-Origin","*");
+  response.send('hello cors')
+})
 
 
 
---
+++ 00.ajax入门/跨域/2.JSONP/js/app.js
@@ -0,0 +1,5 @@
+const data={
+  name:'hawkins'
+}
+
+console.log(data)
